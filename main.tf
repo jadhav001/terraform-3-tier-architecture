6705f96598068faeedb1f4b6b18ddfeb04d66d0f
jadhav001 <147920870+jadhav001@users.noreply.github.com>
2024-09-29 17:59:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "aws_route_table_association" "b" {
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0e54eba7c51c234f6"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "tejesh"
@@ -149,7 +149,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0e54eba7c51c234f6"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "tejesh"
@@ -164,7 +164,7 @@ resource "aws_instance" "webserver2" {
 
 #Create EC2 Instance
 resource "aws_instance" "appserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0e54eba7c51c234f6"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "tejesh"
@@ -176,7 +176,7 @@ resource "aws_instance" "appserver1" {
 }
 
 resource "aws_instance" "appserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0e54eba7c51c234f6"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "tejesh"
